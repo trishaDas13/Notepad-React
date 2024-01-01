34dccf7bfe2cf62ba13aeb8e58b4c3daba638dfe
trishaDas13 <trisha.das1308@gmail.com>
2024-01-01 17:58:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import Split from "react-split";
+import { nanoid } from "nanoid";
+import { useEffect, useState } from "react";
+import './index.css'
+import Sidebar from "./comps/Sidebar";
+import Editor from "./comps/Editor";
 
 function App() {
+  const [notes, setNotes] = useState(
+    () => JSON.parse(localStorage.getItem("notes")) || []
+  );
+  const [currentNoteId, setCurrentNoteId] = useState(
+    (notes[0] && notes[0].id) || ""
+  );
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: `# Enter title here \n\n`,
+    };
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+    setCurrentNoteId(newNote.id);
+  }
+
+  function updateNote(text) {
+    setNotes((oldNotes) => {
+      let arr = [];
+      for (let i = 0; i < oldNotes.length; i++) {
+        if (oldNotes[i].id === currentNoteId) {
+          arr.unshift({ ...oldNotes[i], body: text });
+        } else {
+          arr.push(oldNotes[i]);
+        }
+      }
+      return arr;
+    });
+  }
+
+  function deleteNote(event, noteId) {
+    event.stopPropagation();
+    setNotes((oldNotes) => oldNotes.filter((note) => note.id !== noteId));
+  }
+
+  function findCurrentNote() {
+    return (
+      notes.find((note) => {
+        return note.id === currentNoteId;
+      }) || notes[0]
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {notes.length > 0 ? (
+         <Split 
+         sizes={[15,85]} 
+         direction="horizontal" 
+         className='split'
+       >
+         <Sidebar 
+           notes={notes} 
+           setCurrentNoteId={setCurrentNoteId} 
+           currentNote={findCurrentNote()} 
+           newNote={createNewNote} 
+           deleteNote={deleteNote}
+         />
+         {currentNoteId && notes.length > 0 && 
+            <Editor 
+              updateNote={updateNote} 
+              currentNote={findCurrentNote()}
+            />
+          }
+        </Split>):
+       
+       
+       (
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button className="first-note" onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )
+      }
+    </>
   );
 }
 
